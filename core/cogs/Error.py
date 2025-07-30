03b4ac45b090d681857f7df974a0ef039f7fb9ee
Joshua Deason <joshua.deason@zoho.com>
2025-07-30 01:33:47 -0600

    Add handler for expired pagination error

--- core/cogs/Error.py
+++ core/cogs/Error.py
@@ -34,6 +34,8 @@ class ErrorCog(commands.Cog):
         elif isinstance(error, CrateNotInDatabaseError):
             if ctx.selected_options:
                 await ctx.respond(f"Crate: `{ctx.selected_options[0]["value"]}` is not valid. Please use the Auto Complete suggestions!")
+        elif isinstance(error, discord.Forbidden):
+            print("Invalid Pagination Status.")
         else:
             raise error
     
