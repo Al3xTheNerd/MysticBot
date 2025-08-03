669574fcf819c6fe02eb8bbac9efcab4e673bff9
Joshua Deason <joshua.deason@zoho.com>
2025-08-03 15:26:33 -0600

    Add devmode shit

--- core/env.py
+++ core/env.py
@@ -8,3 +8,9 @@ webAddress = os.environ.get('WEBADDRESS')
 itemSoloAddress = os.environ.get('ITEMSOLOADDRESS')
 itemImageAddress = os.environ.get('ITEMIMAGEADDRESS')
 databaseFile = os.environ.get('DATABASEFILE')
+
+devMode: str | None = os.environ.get('DEV')
+if devMode == "True":
+    DEV_MODE: bool = True
+else:
+    DEV_MODE = False
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import discord
-
-from core.env import token
+import discord, logging
+from core.env import token, DEV_MODE
 import core.db as db
 
 cogs = [
@@ -9,6 +8,17 @@ cogs = [
     "ItemSearch",
     "Misc"
 ]
+
+logger = logging.getLogger('discord')
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+if DEV_MODE:
+    debugLevel = logging.DEBUG
+else:
+    debugLevel = logging.WARNING
+logger.setLevel(debugLevel)
+
 
 bot = discord.Bot(
     intents = discord.Intents.none(),
@@ -27,7 +37,11 @@ for cog in cogs:
 @bot.event
 async def on_ready():
     if bot.user:
-        print(f"Logged in as {bot.user.name} with Pycord [v{discord.__version__}]")
+        if DEV_MODE:
+            await bot.sync_commands(force = True)
+            print(DEV_MODE)
+        print(f"""Logged in as {bot.user.name} with Pycord [v{discord.__version__}]\nDeveloper Mode: {DEV_MODE}\nCommand Count: {len(bot.commands)}
+              """)
 
 
 bot.run(token)
