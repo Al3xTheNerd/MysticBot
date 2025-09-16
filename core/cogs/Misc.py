0268370d6f4c54899bfe638c907e1e477352c306
Joshua Deason <joshua.deason@zoho.com>
2025-09-15 21:56:30 -0600

    Add popularity command

--- core/cogs/Misc.py
+++ core/cogs/Misc.py
@@ -13,9 +13,30 @@ class Misc(commands.Cog):
     def __init__(self, bot):
         self.bot: discord.Bot = bot
     
-    
-    
-    
+    @commands.slash_command(
+        name = "popularity",
+        description = "Shows a list of some of the most popular searched items!")
+    async def topItems(self,
+                       ctx: discord.ApplicationContext):   
+        currentItemCounter = await getItemCounter()
+        returnText = "```ansi\n"
+        if currentItemCounter:
+            mostPopular = list(dict(sorted(currentItemCounter.items(), key=lambda item: item[1])).keys())
+            mostPopular.reverse()
+            for counter, itemName in enumerate(mostPopular, 1):
+                potentialAddition = f"\u001b[0;32m{counter:>3}\u001b[0;0m - \u001b[0;35m{itemName:<40}\u001b[0;0m (\u001b[0;36m{currentItemCounter[itemName]}\u001b[0;0m)\n"
+                if len(returnText + potentialAddition) <= 4096:
+                    returnText += potentialAddition
+                else: break
+            returnText += "```"
+        else:
+            returnText += "No items searched yet!```"
+        
+        embed = discord.Embed(color=0x3c7186)
+        embed.title = "Most popular searches!"
+        embed.description = returnText
+        await ctx.respond(embed = embed)
+        
     @commands.slash_command(
         name = "stats",
         description = "Stats and general info about the bot!")
