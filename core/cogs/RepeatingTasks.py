1b9136aa5be2503880fed4fa6255d0a94f944906
Joshua Deason <joshua.deason@zoho.com>
2026-03-16 20:06:36 -0600

    Bot will check for new item data every 30 minutes now.

--- core/cogs/RepeatingTasks.py
+++ core/cogs/RepeatingTasks.py
@@ -17,60 +17,13 @@ class RepeatingTasksCog(commands.Cog):
         
     @commands.Cog.listener()
     async def on_ready(self):
-        await self.pullData.start()
+        self.pullData.start()
 
-    @tasks.loop(minutes=1)
-    async def updateStatus(self):
-        rand = random.randint(1, 4)
-        status = ""
-        match rand:
-            case 1:
-                cratesList = await db.getCrateList()
-                if cratesList:
-                    crateToUse = random.choice(cratesList)
-                    options = [
-                        f"{crateToUse.CrateName} came out {crateToUse.ReleaseDate}!"
-                    ]
-                    status = random.choice(options)
-            case 2:
-                tagsList = await db.getTagList()
-                if tagsList:
-                    tagToUse = random.choice(tagsList)
-                    options = [
-                        f"Thinking about {tagToUse}s."
-                    ]
-                    status = random.choice(options)
-            case 3:
-                itemsList = await db.getItemList()
-                if itemsList:
-                    itemToUse = random.choice(itemsList)
-                    for symbol in symbolsToRemove:
-                        itemToUse.ItemName = itemToUse.ItemName.replace(f"{symbol} ", "").replace(f" {symbol}", "")
-                    options = [
-                        f"{itemToUse.ItemName} is pretty cool.",
-                        f"{itemToUse.ItemName} - {itemToUse.WinPercentage}%"
-                    ]
-                    status = random.choice(options)
-            case 4:
-                options = [
-                    "Am I alive?",
-                    "Playin' with fire.",
-                    "Not a nerd!",
-                    "Made by Alex!",
-                    "Yo!"
-                ]
-                status = random.choice(options)
-        await self.bot.change_presence(activity = discord.Activity(name = "custom", state = f"{status}", type=discord.ActivityType.custom),
-                                       status = discord.Status.online)
-        
-    
-    @tasks.loop(minutes=3, reconnect=False)
+
+    @tasks.loop(minutes=30)
     async def pullData(self):
         print("Refreshing Data.")
-        if self.updateStatus.is_running():
-            self.updateStatus.stop()
         await updateFromSite()
-
-        await self.updateStatus.start()
+        
 def setup(bot: discord.Bot):
     bot.add_cog(RepeatingTasksCog(bot))
